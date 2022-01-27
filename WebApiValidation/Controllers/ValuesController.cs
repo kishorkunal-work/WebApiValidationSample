9747979aad5abf4a21054f863c66bce63bfd363c
kishor kunal <kkunal@ciena.com>
2022-01-27 13:44:41 +0530

    first commit

---
+++ WebApiValidation/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebApiValidation.Helper;
+using WebApiValidation.Models;
+
+namespace WebApiValidation.Controllers
+{
+	public class ValuesController : ApiController
+	{
+        [HttpPost]
+        [ValidateModel]
+        public HttpResponseMessage UserRegistration(User userModel)
+        {
+            // Note: since we have annotated model with respective validation,
+            // no need to check for ModelState here as that will be taken care by [ValidateModel]
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage IsGuid(string guid)
+        {
+            // Note: since we have premitive parameter here ,
+            // we need to check for ModelState here
+            if (!Guid.TryParse(guid , out Guid g))
+			{
+                ModelState.AddModelError("Parameter guid", "Invalid value for guid");
+                return Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest, ModelState);
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ WebApiValidation/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiValidation.Models
+{
+	public class User
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The client name can not be empty")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Client Name field must have minimum 5 and maximum 15 character!")]
+        public string ClientName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "ClientPassword  cannot be empty!")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "ClientPassword must have minimum 5 and maximum 15 character!")]
+        public string ClientPassword { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm ClientPassword cannot be empty!")]
+        public string ConfirmClientPassword { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "EmailID address  cannot be empty!")]
+        public string EmailID { get; set; }
+    }
+}
